ac57bd1625b02b8457eda62a0fbf635c4efcd0ed
PaGarPa <pacogarciapatino@gmail.com>
2026-02-25 20:42:40 +0100

    Cambios en index.js 2

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-
-const fmgp = require('./index-FMGP.js');
-
 app.get('/samples/FMGP', (req, res) => {
     try {
        
