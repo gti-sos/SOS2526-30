00c47266ee626e00beb5db897f31bfa8368b1dbd
gonzaloogrcia <gggomez234@gmail.com>
2026-03-11 11:19:28 +0100

    GGG

--- index.js
+++ index.js
@@ -1,52 +1,51 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
+    import express from 'express';
+    import path from 'path';
+    import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
 
-const FMPG = require("./index-FMGP.js");
-const GGG = require("./index-GGG.js");
-const MRT = require("./index-MRT.js");
+    //const MRT = require("./index-MRT.js");
 
-import athleteEventsAPI from './api/indexGGG.js';
-import cheatersStatsAPI from './api/cheaters-stats.js';
-import esportsgrowthAPI from './api/esportsgrowth-stats.js';
-import esportsearningsAPI from './api/esportsearnings-stats.js';
+    import {loadBackendGGG} from './src/backend/indexGGG.js';
+    import cheatersStatsAPI from './api/cheaters-stats.js';
+    import esportsgrowthAPI from './api/esportsgrowth-stats.js';
+    import esportsearningsAPI from './api/esportsearnings-stats.js';
 
-const app = express();
-const BASE_URL_API = "/api/v1";
+    const app = express();
+    const BASE_URL_API = "/api/v1";
 
-app.use(express.json());
-app.use(express.static("public"));
+    loadBackendGGG(app)
+
+    app.use(express.json());
+    app.use(express.static("public"));
 
 
-app.use(`${BASE_URL_API}/cheaters-stats`, cheatersStatsAPI);
-app.use(`${BASE_URL_API}/esportsgrowth-stats`, esportsgrowthAPI);
-app.use(`${BASE_URL_API}/esportsearnings-stats`, esportsearningsAPI);
-app.use(`${BASE_URL_API}/olympics-athlete-events`, athleteEventsAPI);
+    app.use(`${BASE_URL_API}/cheaters-stats`, cheatersStatsAPI);
+    app.use(`${BASE_URL_API}/esportsgrowth-stats`, esportsgrowthAPI);
+    app.use(`${BASE_URL_API}/esportsearnings-stats`, esportsearningsAPI);
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-});
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, 'public/index.html'));
+    });
 
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/about.html'));
-});
+    app.get('/about', (req, res) => {
+        res.sendFile(path.join(__dirname, 'public/about.html'));
+    });
 
-app.get("/samples/MRT", (req, res) => {
-    try {
-        const resultado = MRT.calcularMediaEsports();
-        res.send(`<h1>Resultado eSports</h1>
-                   <p>${resultado}</p>`);
-    } catch (error) {
-        res.status(500).send("Error calculando la media de eSports");
-    }
-});
+    // app.get("/samples/MRT", (req, res) => {
+    //     try {
+    //         const resultado = MRT.calcularMediaEsports();
+    //         res.send(`<h1>Resultado eSports</h1>
+    //                 <p>${resultado}</p>`);
+    //     } catch (error) {
+    //         res.status(500).send("Error calculando la media de eSports");
+    //     }
+    // });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log("=".repeat(50));
-    console.log(`Servidor corriendo en puerto ${PORT}`);
-    console.log("=".repeat(50));
-});
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log("=".repeat(50));
+        console.log(`Servidor corriendo en puerto ${PORT}`);
+        console.log("=".repeat(50));
+    });
