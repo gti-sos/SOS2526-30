7e98355d3576e9e083e469db98582079cbdef23e
gonzaloogrcia <gggomez234@gmail.com>
2026-03-18 12:39:38 +0100

    Svelte3

--- index.js
+++ index.js
@@ -30,9 +30,9 @@
     app.use(`${BASE_URL_API}/esportsgrowth-stats`, esportsgrowthAPI);
     app.use(`${BASE_URL_API}/esportsearnings-stats`, esportsearningsAPI);
 
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public/index.html'));
-    });
+    // app.get('/', (req, res) => {
+    //     res.sendFile(path.join(__dirname, 'public/index.html'));
+    // });
 
     app.get('/about', (req, res) => {
         res.sendFile(path.join(__dirname, 'public/about.html'));
