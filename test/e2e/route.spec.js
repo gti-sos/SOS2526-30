a3ae0c1ed88a11e643785a8f1b7d1582ceabc710
gonzaloogrcia <gggomez234@gmail.com>
2026-03-27 12:49:27 +0100

    incio e2e2

--- test/e2e/route.spec.js
+++ test/e2e/route.spec.js
@@ -1,27 +1,25 @@
-// @ts-check
-import { test, expect } from '@playwright/test';
+// // @ts-check
+// import { test, expect } from '@playwright/test';
 
-let app = 'sos2526-30.onrender.com';
+// let app = 'sos2526-30.onrender.com';
 
-test('has title', async ({ page }) => {
-  await page.goto(app);
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/SOS2526-30 Inicio/);
-});
- test('prueba link', async ({ page }) => {
-  await page.goto(app);
-  await page.getByRole('link', { name: 'Acerca de' }).click();
-  await expect(page).toHaveTitle(/SOS2526-30 Sobre Nosotros/);
-});
+// test('has title', async ({ page }) => {
+//   await page.goto(app);
+//   await expect(page).toHaveTitle(/SOS2526-30 Inicio/);
+// });
+//  test('prueba link', async ({ page }) => {
+//   await page.goto(app);
+//   await page.getByRole('link', { name: 'Acerca de' }).click();
+//   await expect(page).toHaveTitle(/SOS2526-30 Sobre Nosotros/);
+// });
 
 
 
-test('prueba api ggg', async ({ page }) => {
-  const athletes = page.locator('.athlete-card');
-  await page.goto(app);
-  await page.getByRole('link', { name: 'Gonzalo - Olympics Athlete Events (v2)' }).click();
-  await page.getByRole('button', { name: 'Cargar Datos Ejemplo' }).click();
-  await expect(page.locator('#itemsPerPageTop')).toHaveJSProperty('value', '5');
-  await expect(athletes).not.toHaveCount(0)
-});
+// test('prueba api ggg', async ({page}) => {
+//   const athletes = page.locator('.athlete-card');
+//   await page.goto('https://sos2526-30.onrender.com/olympics-athlete-events');
+//   await page.getByRole('link', { name: 'Gonzalo - Olympics Athlete Events (v2)' }).click();
+//   await page.getByRole('button', { name: 'Cargar Datos Ejemplo' }).click();
+//   await expect(page.locator('#itemsPerPageTop')).toHaveJSProperty('value', '5');
+//   await expect(athletes).not.toHaveCount(0)
+// });
