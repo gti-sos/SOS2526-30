4cc4c94185fb0d9e33eea3e1d0ef4dcf6e6ba23e
gonzaloogrcia <gggomez234@gmail.com>
2026-04-29 18:23:18 +0200

    integracion prueba

--- src/front/vite.config.js
+++ src/front/vite.config.js
@@ -1,8 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import dotenv from 'dotenv';
 
-dotenv.config();
 
 export default defineConfig({
     plugins: [sveltekit()],
