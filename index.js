fc8d6db187af5f25aeac92f1951e6be29577ed46
davreaper <146114507+davreaper@users.noreply.github.com>
2026-05-05 18:12:19 +0200

    drp

--- index.js
+++ index.js
@@ -14,7 +14,7 @@
     import loadCheatersStatsGithubOAuth from './src/backend/cheaters-stats-github-oauth.js';
     import loadTwitchProxy from './src/backend/twitch-proxy.js';
     import loadSpotifyProxy from './src/backend/spotify-proxy.js';
-//    import { loadCitysStatsProxy } from './src/backend/citys-stats-proxy.js';
+    import  loadCitysStatsProxy  from './src/backend/citys-stats-proxy.js';
 
 
 import loadBackendGGG from './src/backend/olympics-athlete-events.js';
@@ -34,11 +34,9 @@ app.use('/api/github', githubGGGProxy);
 loadCheatersStatsGithubOAuth(app);
 loadTwitchProxy(app);
 loadSpotifyProxy(app);
-
+loadCitysStatsProxy(app);
 loadBackendGGG(app);
 loadBackendFMGP(app);
-
-//loadCitysStatsProxy(app); 
 
 app.use(`${BASE_URL_API}/esportsgrowth-stats`, esportsgrowthAPI);
 app.use(`${BASE_URL_API}/esportsearnings-stats`, esportsearningsAPIv1);
